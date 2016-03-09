b12982f82f932ee328706f1a78219e3a9e2d4fcd
Maruen Mehana <maruen@gmail.com>
2016-03-09 16:36:59 -0300

    README Improvements

--- scripts/insertMap.sh
+++ scripts/insertMap.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl    -H "Content-Type: application/json" -X POST --data @insertMap.json --silent http://localhost:9000/insertMap
+curl    -H "Content-Type: application/json" -X POST --data @insertMap.json --silent http://45.56.121.7:9000/insertMap
 echo ""
