8c864fcbb5e38f64484d66af719df107329cfe16
Maruen Mehana <maruen@gmail.com>
2016-03-09 14:50:42 -0300

    More Progress

--- app/services/RouteService.java
+++ app/services/RouteService.java
@@ -10,7 +10,6 @@ import java.text.NumberFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map.Entry;
 
--- start.sh
+++ start.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-if [  "$1" == "devel"  ]; then
-	echo "[info] Starting server in development mode"
-	JAVA_OPTS="-Dhttps.port=9443
-		        -Dhttp.port=9000
-		        -Dhttps.keyStore=conf/generated.keystore"  activator -jvm-debug 9999 run
-else
+if [  "$1" == "prod"  ]; then
 	echo "[info] Backing up database files..."
-  	${WALLMART_EXAM_HOME}/scripts/dumpAll.sh
+  	${WALLMART_HOME}/scripts/dumpAll.sh
 	
 	echo "[info] Starting server in production mode"
 	JAVA_OPTS="-Dhttps.port=443
 		  		  -Dhttp.port=80
 		        -DapplyEvolutions.default=true
 		  		  -DapplyDownEvolutions.default=true" activator start
+else
+		echo "[info] Starting server in development mode"
+		JAVA_OPTS="-Dhttps.port=9443
+		        -Dhttp.port=9000
+		        -Dhttps.keyStore=conf/generated.keystore"  activator -jvm-debug 9999 run
+
 fi
